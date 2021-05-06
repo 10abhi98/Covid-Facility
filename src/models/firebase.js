48ea1f14e9fc43662dd837869f7ca3cbae5ab8ea
nitika.agarwal <nitika.agarwal@quillbot.com>
2021-05-06 14:09:37 +0530

    Added: Firebase firestore CRUD Operations reference

--- src/models/firebase.js
+++ src/models/firebase.js
@@ -1,23 +1,23 @@
 // Firebase imports
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const provider = new firebase.auth.GoogleAuthProvider();
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCBM1E-ugDaw3X1xTi_NBOCYkaG-ffpGRg",
-  authDomain: "covid-facility.firebaseapp.com",
-  projectId: "covid-facility",
-  storageBucket: "covid-facility.appspot.com",
-  messagingSenderId: "1019911003749",
-  appId: "1:1019911003749:web:cda843bf4cd5cffcb4b08c",
-  measurementId: "G-1205BK95B0",
+  apiKey: 'AIzaSyCBM1E-ugDaw3X1xTi_NBOCYkaG-ffpGRg',
+  authDomain: 'covid-facility.firebaseapp.com',
+  projectId: 'covid-facility',
+  storageBucket: 'covid-facility.appspot.com',
+  messagingSenderId: '1019911003749',
+  appId: '1:1019911003749:web:cda843bf4cd5cffcb4b08c',
+  measurementId: 'G-1205BK95B0',
 };
 
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+export const firestore = firebase.firestore(); // Creating cloud firestore instance
 
 export const signInWithGoogle = () => {
   auth.signInWithPopup(provider);
